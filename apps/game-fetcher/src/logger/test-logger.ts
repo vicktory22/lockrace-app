eefcf0c5d029843ed1f602775be88c59aed828e9
Tim Vick <t.vick@yahoo.com>
2023-07-08 16:34:06 -0700

    chore: remove old logger files

--- apps/game-fetcher/src/logger/test-logger.ts
+++
@@ -1,6 +0,0 @@
-import { type Logger } from "./logger-manager";
-
-export const testLogger = (): Logger => ({
-	info: (_message: string): Promise<void> => Promise.resolve(),
-	error: (_message: Error): Promise<void> => Promise.resolve(),
-});
