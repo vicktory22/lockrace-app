291ee5fde463f74249064e8b2d3fad86b44f3052
Tim Vick <t.vick@yahoo.com>
2023-07-22 12:11:13 -0700

    chore: update boundary to take full config

--- packages/logger/src/helpers.ts
+++ packages/logger/src/helpers.ts
@@ -1,55 +1,78 @@
-type BuildPayloadInput = {
-  message: string | Error;
-  level: string;
-  nanoseconds: string;
-  appName: string;
+import { errAsync, fromPromise, okAsync } from "neverthrow";
+import {
+	BuildPayloadParams,
+	BuildPayloadResult,
+	DispatchInput,
+	DispatchResult,
+	PostLogParams,
+	PostLogResult,
+} from "./types";
+
+export function dispatch({
+	message,
+	level,
+	config,
+}: DispatchInput): DispatchResult {
+	const nanoseconds = (Date.now() * 1e6).toString();
+	const { externalUrl, externalUser, externalApiKey, appName } = config;
+
+	return postLog({
+		url: externalUrl,
+		authToken: createToken(externalUser, externalApiKey),
+		message: buildPayload({ message, level, nanoseconds, appName }),
+	})
+		.andThen((res) => {
+			if (!res.ok) {
+				return errAsync(
+					new Error(
+						`Invalid response received from Loki logging service: [${res.status} - ${res.statusText}]`,
+					),
+				);
+			}
+
+			return okAsync(res);
+		})
+		.match(
+			() => {},
+			(err) => {
+				throw err;
+			},
+		);
 }
 
-export type LoggerConfig = {
-  logToUrl: string;
-  authToken: string;
-  appName: string;
+function postLog({ message, authToken, url }: PostLogParams): PostLogResult {
+	return fromPromise(
+		fetch(url, {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+				authorization: `Basic ${authToken}`,
+			},
+			body: JSON.stringify(message),
+		}),
+		(cause) => new Error("Error sending log to logging service.", { cause }),
+	);
 }
 
-export type Logger = {
-  info: (message: string) => Promise<void>;
-  error: (message: Error) => Promise<void>;
-};
+export function buildPayload({
+	message,
+	level,
+	nanoseconds,
+	appName,
+}: BuildPayloadParams): BuildPayloadResult {
+	return {
+		streams: [
+			{
+				stream: {
+					level,
+					app: appName,
+				},
+				values: [[nanoseconds, message]],
+			},
+		],
+	};
+}
 
-const buildPayload = ({ message, level, nanoseconds, appName }: BuildPayloadInput) => ({
-  streams: [
-    {
-      stream: {
-        level,
-        app: appName,
-      },
-      values: [[nanoseconds, message]],
-    },
-  ],
-});
-
-export const dispatch = async (
-  message: string | Error,
-  level: string,
-  config: LoggerConfig,
-): Promise<void> => {
-  const nanoseconds = (Date.now() * 1e6).toString();
-
-  const fetchResult = await fetch(config.logToUrl, {
-    headers: {
-      method: "POST",
-      "content-type": "application/json",
-      authorization: `Basic ${config.authToken}`,
-    },
-    body: JSON.stringify(buildPayload({ message, level, nanoseconds, appName: config.appName })),
-  })
-  .catch((error: unknown) => new Error('Network error', { cause: error }));
-
-  if (fetchResult instanceof Error) {
-    throw fetchResult;
-  }
-
-  if (!fetchResult.ok) {
-    throw new Error(`Invalid response received from Loki logging service: [${fetchResult.status} - ${fetchResult.statusText}]`);
-  }
-};
+function createToken(user: string, apiKey: string): string {
+	return btoa(`${user}:${apiKey}`);
+}
