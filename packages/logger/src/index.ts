eae619a38df34070bbc3f547ed987f71e60df35e
Tim Vick <t.vick@yahoo.com>
2023-07-22 12:11:50 -0700

    chore: implement new config setup

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,8 +1,20 @@
-import { LoggerConfig, Logger, dispatch } from "./helpers";
+import { dispatch } from "./helpers";
 
-export const getLogger = (config: LoggerConfig): Logger => {
-  return {
-    info: (message: string) => dispatch(message, "info", config),
-    error: (message: Error) => dispatch(message, "error", config),
-  };
+export type LoggerConfig = {
+	appName: string;
+	externalUrl: string;
+	externalUser: string;
+	externalApiKey: string;
 };
+
+export type Logger = {
+	info: (message: string) => Promise<void>;
+	error: (message: Error) => Promise<void>;
+};
+
+export function getLogger(config: LoggerConfig): Logger {
+	return {
+		info: (message: string) => dispatch({ message, level: "info", config }),
+		error: (message: Error) => dispatch({ message, level: "error", config }),
+	};
+}
