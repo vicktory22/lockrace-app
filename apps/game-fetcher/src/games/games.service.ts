a2c180efb0557129868431126e3776d58a1f374b
Tim Vick <t.vick@yahoo.com>
2023-07-22 14:19:54 -0700

    chore: hookup neverthrow package

--- apps/game-fetcher/src/games/games.service.ts
+++ apps/game-fetcher/src/games/games.service.ts
@@ -1,21 +1,19 @@
-import { Result } from "@sniptt/monads";
-import { tryCatchPromise } from "../helpers/try-catch";
-import { FetchGamesResponse } from "../schemas/fetch-games.schema";
-import { GetGamesResponse } from "../types";
+import { ResultAsync, fromPromise, okAsync } from "neverthrow";
+import { GetGamesResponse } from "./types";
 import { fetchWithTimeout } from "../utils/fetcher";
 import { GetGamesError } from "./games.errors";
-import { handleBadRequest, parseGamesResponse } from "./games.helpers";
+import {
+	handleBadRequest,
+	parseGamesResponse,
+	parseGamesResponseJson,
+	validateGamesResponse,
+} from "./games.helpers";
 
-export const getGames = async (
+export const getGames = (
 	gamesUrl: string,
-): Promise<Result<GetGamesResponse, GetGamesError>> => {
-	const fetchResult = await tryCatchPromise(
-		fetchWithTimeout(gamesUrl)
-			.then(handleBadRequest)
-			.then((response) => response.json())
-			.then(FetchGamesResponse.parseAsync),
-		(err) => GetGamesError.from(err),
-	);
-
-	return fetchResult.map(parseGamesResponse);
-};
+): ResultAsync<GetGamesResponse, GetGamesError> =>
+	fromPromise(fetchWithTimeout(gamesUrl), (err) => GetGamesError.from(err))
+		.andThen(handleBadRequest)
+		.andThen(parseGamesResponseJson)
+		.andThen(validateGamesResponse)
+		.andThen((r) => okAsync(parseGamesResponse(r)));
