653720f1e1a3c1ac94e339e5ad4a3aea8a181164
Tim Vick <t.vick@yahoo.com>
2023-07-28 08:44:50 -0700

    chore: update tests for new api

--- apps/api/src/games/__tests__/games.service.test.ts
+++ apps/api/src/games/__tests__/games.service.test.ts
@@ -1,21 +1,29 @@
 import { describe, expect, it } from "@jest/globals";
+import assert from "node:assert";
 import { fetchGames } from "../games.service";
+import { DatabaseError } from "../../database/database.errors";
 
 describe("fetchGames", () => {
 	it("should return empty array if there is no context passed in", async () => {
 		const result = await fetchGames(undefined);
-		expect(result).toEqual([]);
+
+		assert(result instanceof DatabaseError);
+		expect(result.message).toBe("No database found.");
 	});
 
 	it("should return empty array if there is no env", async () => {
 		// @ts-ignore
 		const result = await fetchGames({ env: undefined });
-		expect(result).toEqual([]);
+
+		assert(result instanceof DatabaseError);
+		expect(result.message).toBe("No database found.");
 	});
 
 	it("should return empty array if there is no env.DB", async () => {
 		// @ts-ignore
 		const result = await fetchGames({ env: { DB: undefined } });
-		expect(result).toEqual([]);
+
+		assert(result instanceof DatabaseError);
+		expect(result.message).toBe("No database found.");
 	});
 });
