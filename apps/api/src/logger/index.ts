55837d7b89b9a1b45a8912728750063b1c57dc82
Tim Vick <t.vick@yahoo.com>
2023-07-28 08:12:24 -0700

    feat: add logger (#56)

---
+++ apps/api/src/logger/index.ts
@@ -0,0 +1,12 @@
+export function getLogger() {
+
+  return {
+    info: (message: string) => {
+      console.log(message);
+    },
+
+    error: (message: string | Error) => {
+      console.error(message);
+    }
+  };
+}
