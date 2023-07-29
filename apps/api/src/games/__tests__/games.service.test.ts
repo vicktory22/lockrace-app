b129223fa9505d062fa2bd57eac69e59955aa70f
Tim Vick <t.vick@yahoo.com>
2023-07-29 09:25:15 -0700

    feat: add check for time

--- apps/api/src/games/__tests__/games.service.test.ts
+++ apps/api/src/games/__tests__/games.service.test.ts
@@ -1,29 +1,62 @@
-import { describe, expect, it } from "@jest/globals";
+import {
+	jest,
+	describe,
+	expect,
+	it,
+	afterEach,
+	beforeEach,
+} from "@jest/globals";
 import assert from "node:assert";
 import { fetchGames } from "../games.service";
 import { DatabaseError } from "../../database/database.errors";
 
 describe("fetchGames", () => {
-	it("should return empty array if there is no context passed in", async () => {
-		const result = await fetchGames(undefined);
-
-		assert(result instanceof DatabaseError);
-		expect(result.message).toBe("No database found.");
+	afterEach(() => {
+		jest.useRealTimers();
 	});
 
-	it("should return empty array if there is no env", async () => {
-		// @ts-ignore
-		const result = await fetchGames({ env: undefined });
-
-		assert(result instanceof DatabaseError);
-		expect(result.message).toBe("No database found.");
+	beforeEach(() => {
+		jest.useFakeTimers();
 	});
 
-	it("should return empty array if there is no env.DB", async () => {
-		// @ts-ignore
-		const result = await fetchGames({ env: { DB: undefined } });
+	describe("before games are released", () => {
+		beforeEach(() => {
+			jest.setSystemTime(new Date("2021-01-01T08:00:00.000Z"));
+		});
 
-		assert(result instanceof DatabaseError);
-		expect(result.message).toBe("No database found.");
+		it("should return an empty array if it is before 4pm UTC / 9am PST", async () => {
+			const result = await fetchGames(undefined);
+
+			expect(result).toEqual([]);
+		});
+	});
+
+	describe("after games are released", () => {
+		beforeEach(() => {
+			jest.setSystemTime(new Date("2021-01-01T18:00:00.000Z"));
+		});
+
+		it("should return empty array if there is no context passed in", async () => {
+			const result = await fetchGames(undefined);
+
+			assert(result instanceof DatabaseError);
+			expect(result.message).toBe("No database found.");
+		});
+
+		it("should return empty array if there is no env", async () => {
+			// @ts-ignore
+			const result = await fetchGames({ env: undefined });
+
+			assert(result instanceof DatabaseError);
+			expect(result.message).toBe("No database found.");
+		});
+
+		it("should return empty array if there is no env.DB", async () => {
+			// @ts-ignore
+			const result = await fetchGames({ env: { DB: undefined } });
+
+			assert(result instanceof DatabaseError);
+			expect(result.message).toBe("No database found.");
+		});
 	});
 });
