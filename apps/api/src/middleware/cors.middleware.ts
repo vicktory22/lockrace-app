8870ef24e7c600410ac7c8ebea2d6f4dfb860389
Tim Vick <t.vick@yahoo.com>
2023-07-29 15:45:53 -0700

    fix: lint fixes

--- apps/api/src/middleware/cors.middleware.ts
+++ apps/api/src/middleware/cors.middleware.ts
@@ -2,30 +2,34 @@ import { Next } from "hono";
 import { AppContext } from "../types";
 
 export async function corsMiddleware(c: AppContext, next: Next): Promise<void> {
-  const origin = c.req.headers.get("origin");
+	const origin = c.req.headers.get("origin");
 
-  const shouldAddCors = origin && await shouldAddCorsHeader(origin, c?.env?.DB);
+	const shouldAddCors =
+		origin && (await shouldAddCorsHeader(origin, c?.env?.DB));
 
-  await next();
+	await next();
 
-  if (!shouldAddCors) {
-    return;
-  }
+	if (!shouldAddCors) {
+		return;
+	}
 
-  c.header("Access-Control-Allow-Origin", origin);
-  c.header("Access-Control-Allow-Credentials", "true");
+	c.header("Access-Control-Allow-Origin", origin);
+	c.header("Access-Control-Allow-Credentials", "true");
 }
 
-async function shouldAddCorsHeader(origin: string | null, db: KVNamespace | undefined): Promise<boolean> {
-  if (!origin || !db) {
-    return false;
-  }
+async function shouldAddCorsHeader(
+	origin: string | null,
+	db: KVNamespace | undefined,
+): Promise<boolean> {
+	if (!origin || !db) {
+		return false;
+	}
 
-  const allowedOrigins = await db.get("allowed-origins");
+	const allowedOrigins = await db.get("allowed-origins");
 
-  if (!allowedOrigins) {
-    return false;
-  }
+	if (!allowedOrigins) {
+		return false;
+	}
 
-  return allowedOrigins.split(",").includes(origin);
+	return allowedOrigins.split(",").includes(origin);
 }
