7522eca27f67c2bb5ca43a14565d42edaa739a90
Tim Vick <t.vick@yahoo.com>
2023-08-07 08:20:18 -0700

    feat: add fetch weeks function

---
+++ apps/sync/src/weeks/index.ts
@@ -0,0 +1,12 @@
+import { KVNamespace } from "@cloudflare/workers-types";
+import { KVWeek } from "../types";
+
+export async function fetchWeeksFromKV(kv: KVNamespace): Promise<KVWeek[]> {
+  const weeks: string | Error = await kv.get("weeks").catch((err) => err);
+
+  if (weeks instanceof Error) {
+    return [];
+  }
+
+  return JSON.parse(weeks);
+}
