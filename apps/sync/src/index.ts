ac0f64c24979ccb7d9b344492052f4be410613e7
Tim Vick <t.vick@yahoo.com>
2023-08-07 08:35:51 -0700

    chore: return kv values

--- apps/sync/src/index.ts
+++ apps/sync/src/index.ts
@@ -1,17 +1,20 @@
 import {
-	ExecutionContext,
-	KVNamespace,
-	Request as WorkerRequest,
+  ExecutionContext,
+  KVNamespace,
+  Request as WorkerRequest,
 } from "@cloudflare/workers-types";
+import { fetchWeeksFromKV } from "./weeks";
 
 export interface Env {
-	GAMES_URL: string;
-	FOOTBALL_METADATA: KVNamespace;
-	FOOTBALL_GAMES: KVNamespace;
+  GAMES_URL: string;
+  FOOTBALL_METADATA: KVNamespace;
+  FOOTBALL_GAMES: KVNamespace;
 }
 
 export default {
-	async fetch(_request: WorkerRequest, _env: Env, _ctx: ExecutionContext) {
-		return new Response("Hello World!");
-	},
+  async fetch(_request: WorkerRequest, env: Env, _ctx: ExecutionContext) {
+    const weeks = await fetchWeeksFromKV(env.FOOTBALL_METADATA);
+
+    return new Response(JSON.stringify(weeks));
+
 };
