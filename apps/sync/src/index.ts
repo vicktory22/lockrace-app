1892a6947a8eca6c2fe4b857d7926b580c447ac1
Tim Vick <t.vick@yahoo.com>
2023-08-07 08:41:19 -0700

    fix: formatting fixes

--- apps/sync/src/index.ts
+++ apps/sync/src/index.ts
@@ -1,20 +1,20 @@
 import {
-  ExecutionContext,
-  KVNamespace,
-  Request as WorkerRequest,
+	ExecutionContext,
+	KVNamespace,
+	Request as WorkerRequest,
 } from "@cloudflare/workers-types";
 import { fetchWeeksFromKV } from "./weeks";
 
 export interface Env {
-  GAMES_URL: string;
-  FOOTBALL_METADATA: KVNamespace;
-  FOOTBALL_GAMES: KVNamespace;
+	GAMES_URL: string;
+	FOOTBALL_METADATA: KVNamespace;
+	FOOTBALL_GAMES: KVNamespace;
 }
 
 export default {
-  async fetch(_request: WorkerRequest, env: Env, _ctx: ExecutionContext) {
-    const weeks = await fetchWeeksFromKV(env.FOOTBALL_METADATA);
+	async fetch(_request: WorkerRequest, env: Env, _ctx: ExecutionContext) {
+		const weeks = await fetchWeeksFromKV(env.FOOTBALL_METADATA);
 
-    return new Response(JSON.stringify(weeks));
+		return new Response(JSON.stringify(weeks));
 
 };
--- apps/sync/src/weeks/index.ts
+++ apps/sync/src/weeks/index.ts
@@ -2,11 +2,11 @@ import { KVNamespace } from "@cloudflare/workers-types";
 import { KVWeek } from "../types";
 
 export async function fetchWeeksFromKV(kv: KVNamespace): Promise<KVWeek[]> {
-  const weeks: string | Error = await kv.get("weeks").catch((err) => err);
+	const weeks: string | Error = await kv.get("weeks").catch((err) => err);
 
-  if (weeks instanceof Error) {
-    return [];
-  }
+	if (weeks instanceof Error) {
+		return [];
+	}
 
-  return JSON.parse(weeks);
+	return JSON.parse(weeks);
 }
