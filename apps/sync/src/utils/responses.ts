575e9b08ca7314ce18b046dafb197f27c3e5c987
Tim Vick <t.vick@yahoo.com>
2023-08-11 11:02:04 -0700

    fix: update formatting

--- apps/sync/src/utils/responses.ts
+++ apps/sync/src/utils/responses.ts
@@ -1,8 +1,8 @@
 export const reply = {
-	internalServerError: (message: string) => {
-		console.log({ error: message });
-		return new Response(JSON.stringify({ error: message }), { status: 500 });
-	},
+  internalServerError: (message: string) => {
+    console.log({ error: message });
+    return new Response(JSON.stringify({ error: message }), { status: 500 });
+  },
 
-	ok: (body: string) => new Response(body, { status: 200 }),
+  ok: (body: string) => new Response(body, { status: 200 }),
 };
