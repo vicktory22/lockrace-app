a4dbb7d12f1ef4c60b1af3404eb15539bd1d4f27
Tim Vick <t.vick@yahoo.com>
2023-08-11 10:22:32 -0700

    feat: add kv wrapper

---
+++ apps/sync/src/utils/kv.ts
@@ -0,0 +1,6 @@
+import { KVNamespace } from "@cloudflare/workers-types";
+import { encaseAsync } from "./result";
+
+export async function fetchKV(kv: KVNamespace, key: string) {
+	return encaseAsync(kv.get(key));
+}
