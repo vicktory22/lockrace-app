8d6498fbcf57d9988bdcc3c9ae9434e569e5a0ff
Tim Vick <t.vick@yahoo.com>
2023-08-11 13:22:28 -0700

    feat: add function to get current week (#97)
    * feat: add function to get current week
    
    * feat: add fetch games function

--- apps/sync/src/index.ts
+++ apps/sync/src/index.ts
@@ -1,20 +1,21 @@
-import {
-	ExecutionContext,
-	KVNamespace,
-	Request as WorkerRequest,
-} from "@cloudflare/workers-types";
-import { fetchWeeksFromKV } from "./weeks";
+import { ExecutionContext, KVNamespace, Request as WorkerRequest } from "@cloudflare/workers-types";
+import { fetchGames } from "./games/fetch-games";
+import { reply } from "./utils/responses";
 
 export interface Env {
-	GAMES_URL: string;
-	FOOTBALL_METADATA: KVNamespace;
-	FOOTBALL_GAMES: KVNamespace;
+  GAMES_URL: string;
+  FOOTBALL_METADATA: KVNamespace;
+  FOOTBALL_GAMES: KVNamespace;
 }
 
 export default {
-	async fetch(_request: WorkerRequest, env: Env, _ctx: ExecutionContext) {
-		const weeks = await fetchWeeksFromKV(env.FOOTBALL_METADATA);
+  async fetch(_request: WorkerRequest, env: Env, _ctx: ExecutionContext) {
+    const fetchResult = await fetchGames(env);
 
-		return new Response(JSON.stringify(weeks));
+    if (!fetchResult.ok) {
+      return reply.internalServerError(fetchResult.error.message);
+    }
+
+    return reply.ok(JSON.stringify({ games: fetchResult.value }));
   },
 };
