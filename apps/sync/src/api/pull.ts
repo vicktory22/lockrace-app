64b749f777ceca1917965c34ffa5315af7375ea3
Tim Vick <t.vick@yahoo.com>
2023-08-12 09:15:16 -0700

    feat: pull and display schedule endpoint (#102)
    * feat: adds logic to fetch and parse schedule
    
    * feat: adds ability to fetch and display the schedule

--- apps/sync/src/api/pull.ts
+++ apps/sync/src/api/pull.ts
@@ -1,7 +1,8 @@
 import { Env } from "..";
 import { Err, Ok, Result, encaseAsync } from "../utils/result";
+import { ScheduledPayload } from "./schedule-types";
 
-export async function pull(env: Env): Promise<Result<unknown, Error>> {
+export async function pull(env: Env): Promise<Result<ScheduledPayload, Error>> {
   const url = env.SCOREBOARD_URL;
 
   const apiResult = await encaseAsync(fetch(url).then((res) => res.json()));
