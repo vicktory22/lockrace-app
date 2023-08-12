47acf7c1e7b6bf1f3f93e5743ee387f3b8343b55
Tim Vick <t.vick@yahoo.com>
2023-08-12 10:40:36 -0700

    chore: remove unused code (#106)

--- apps/sync/src/utils/kv.ts
+++
@@ -1,6 +0,0 @@
-import { KVNamespace } from "@cloudflare/workers-types";
-import { encaseAsync } from "./result";
-
-export async function fetchKV(kv: KVNamespace, key: string) {
-  return encaseAsync(kv.get(key));
-}
