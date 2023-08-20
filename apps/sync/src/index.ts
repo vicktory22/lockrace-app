b4e2a83a0acff635ae7bb6ca7121ff86cdb3d953
Tim Vick <t.vick@yahoo.com>
2023-08-20 11:28:28 -0700

    chore: hooks in new games logic from db

--- apps/sync/src/index.ts
+++ apps/sync/src/index.ts
@@ -1,32 +1,36 @@
 import { ExecutionContext, KVNamespace, Request as WorkerRequest } from "@cloudflare/workers-types";
 import { encaseAsync } from "./utils/result";
-import { getGamesFromApi } from "./games/games-service";
-import { KVGame } from "./games/parse-api-types";
+import { getDatabase } from "./db/client";
+import { getGamesFromApi } from "./games/get-games-from-api";
+import { eventsToGames } from "./games/events-to-games";
+import { upsertGames } from "./games/upsert-games";
 
 export interface Env {
   SCOREBOARD_URL: string;
   GAMES_URL: string;
   FOOTBALL_METADATA: KVNamespace;
   FOOTBALL_GAMES: KVNamespace;
+  DB_URL: string;
+  DB_AUTH_TOKEN: string;
 }
 
 export default {
-  async scheduled(_request: WorkerRequest, env: Env, _ctx: ExecutionContext) {
+  async scheduled(_req: WorkerRequest, env: Env, _ctx: ExecutionContext) {
     const gamesResult = await getGamesFromApi(env);
 
     if (!gamesResult.ok) {
+      console.error("Unable to get games from api", { cause: gamesResult.error });
       return;
     }
 
-    const pastGames = await encaseAsync<KVGame[] | null>(env.FOOTBALL_METADATA.get("schedule", { type: "json" }));
-    if (!pastGames.ok) {
-      console.error("Unable to get past games", { cause: pastGames.error });
-      return;
-    }
-    const saveResult = await encaseAsync(env.FOOTBALL_METADATA.put("schedule", JSON.stringify(gamesResult.value)));
+    const newGames = eventsToGames(gamesResult.value);
 
-    if (!saveResult.ok) {
-      console.error(saveResult.error);
+    const db = getDatabase(env.DB_URL, env.DB_AUTH_TOKEN);
+
+    const upsertResult = await encaseAsync(upsertGames(db, newGames));
+
+    if (!upsertResult.ok) {
+      console.error("Unable to upsert games", { cause: upsertResult.error });
       return;
     }
   },
