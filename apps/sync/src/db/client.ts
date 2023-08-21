a98705d68953ee6deb1f9f90d48ae9190639b995
Tim Vick <t.vick@yahoo.com>
2023-08-21 06:57:03 -0700

    fix: adds authToken to db creation method (#113)

--- apps/sync/src/db/client.ts
+++ apps/sync/src/db/client.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
 export function getDatabase(url: string, authToken: string) {
-  let client = createClient({ url });
+  let client = createClient({ url, authToken });
 
   if (process.env.NODE_ENV === "production") {
     client = createClient({ url, authToken });
